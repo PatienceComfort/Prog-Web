7989d3216b4f8c9243c6e500eb03ebe8ed13752a
RJker <71787083+RJker@users.noreply.github.com>
2020-11-08 22:22:01 +0100

    Update Requetes

--- genegate_requete.sql
+++ genegate_requete.sql
@@ -1,76 +1,76 @@
 -- Faire son login:
-SELECT role (ou alors IdUtilisateur)
+SELECT utilisateur.role --Pour savoir quelle vue de la page d'accueil choisir
 FROM utilisateur
-WHERE username = ‘X’ AND mdp = ‘Y’;
+WHERE username = "JeanPierre_2";
+
+SELECT utilisateur.mdp --Pour verifier le mot de passe
+FROM utilisateur
+WHERE username = ‘X’;
+
+SELECT utilisateur.mdp --Pour verifier le mot de passe
+FROM utilisateur
+WHERE email = ‘X’;
 
 -- S'inscrire:
 INSERT INTO utilisateur
-VALUES (‘email.com’, ‘username’, ‘mdp’, ‘nom’, ‘prenom’, ‘numtel’, ‘statut’)
-
--- Recherche d’information sur le gène:
+VALUES (‘email.com’, ‘username’, ‘mdp’, ‘nom’, ‘prenom’, ‘numtel’, ‘statut’); --mettre des vraies valeurs ? 
 
 
-
-Avoir toutes les informations sur un utilisateur :
-
+-- Avoir toutes les informations sur un utilisateur :
 SELECT *
 FROM utilisateur
-WHERE username =MauveG’ 
+WHERE username = "MauveG";
 
-Rechercher les annotateurs:
-
+-- Rechercher les annotateurs:
 SELECT nom,prenom
 FROM utilisateur
-WHERE role = ’Annotateur’
+WHERE role = "Annotateur"
 
 
-Recherche d’information sur une séquence proteique :
-
+-- Recherche d’information sur une séquence proteique :
 SELECT *
 FROM sequences
-WHERE seqP =”MPLLKDBNTRRADETN” 
+WHERE seqProt = "MPLLKDBNTRRADETN";
 
-Recherche d’information sur une séquence nucléotidique:
-
+-- Recherche d’information sur une séquence nucléotidique:
 SELECT seqNt
-FROM genome , sequences
-WHERE genome.idGenome = sequences.idGenome
+FROM sequences
+WHERE seqNt LIKE "%ATAAACCG%"
+  AND fonction= "nuclease";
 
-Recherche d’information sur le génome  de E.Coli :
-
+-- Recherche d’information sur le génome  de E.Coli :
 SELECT *
 FROM genome
-WHERE genre = “Escherichia” and souche =”Coli” 
+WHERE genre = "Escherichia" and souche ="Coli";
 
-Recherche de séquence annoté par Paul :
+-- Recherche de séquence annoté par Paul :
 SELECT seqid
 FROM annotation, utilisateur
-where annotation.emailAnnot = utilisateur.email and utilisateur.prenom =’Paul’
+where annotation.emailAnnot = utilisateur.email and utilisateur.prenom ="Paul";
 
 
-Selectionner Les séquences non attribués 
+-- Selectionner Les séquences non attribués 
 SELECT geneId
 FROM sequences,genome
-WHERE sequences.statut = “0”
+WHERE sequences.statut = "0";
 
 
-Regrouper chez E.Coli les gènes par leurs fonctions :
+-- Regrouper chez E.Coli les gènes par leurs fonctions :
 SELECT fonction
 FROM sequences, genome
-WHERE sequences.idGenome = genome.idGenome and genre = “Escherichia” and souche =”Coli” 
-GROUP BY fonction
-Sélectionner les annotateurs ayant validés des séquences du génome id = “AR5330I” 
-SELECT nom, prenom
-FROM utilisateur , annotation 
-WHERE utilisateur.email = annotation.email 
-and idGenome = 	SELECT idGenome
-			FROM genome
-		WHERE id Genome =  “AR5330I” 
+WHERE sequences.idGenome = genome.idGenome and genre = "Escherichia" and souche ="Coli" 
+GROUP BY fonction;
 
-Trier Les annotateurs ayant annotés le plus de séquences 
+-- Sélectionner les annotateurs ayant validés des séquences du génome id = “AR5330I” 
+SELECT nom, prenom
+FROM utilisateur , annotation
+WHERE utilisateur.email = annotation.email 
+and idGenome =  "AR5330I";
+
+-- Trier Les annotateurs ayant annotés le plus de séquences 
 SELECT emailAnnot, COUNT(idSeq)
 FROM  annotation 
 GROUP BY emailAnnot 
-ORDER BY COUNT(idSeq)
+ORDER BY COUNT(idSeq);
 
-Selectionner les génomes complètement annotés :
+-- Selectionner les génomes complètement annotés :
