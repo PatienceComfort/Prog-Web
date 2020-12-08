091b84d0c17797c61f4695c58e653158826c497e
RJker <71787083+RJker@users.noreply.github.com>
2020-12-08 22:47:12 +0100

    Nouveau Forum

---
+++ ForumDiscussions.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<?php session_start();?>
+<html lang="fr">
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Website Style</title>  
+	    <link rel="stylesheet" type="text/css" href="style1.css">
+	    <div id="header"> <br>
+		    <h> GeneGATE </h> <br><br>					
+	    </div>
+    </head>
+
+    <body>
+        <div id ="menu"> 
+                <?php if ( $_SESSION['statut'] == 'Annotateur') {
+                    $menu='MenuA.php'; 
+            } else if ( $_SESSION['statut'] == 'Validateur') {      	 
+                $menu='MenuV.php'; 
+            } else {
+                $menu='MenuL.php'; 
+            }?>
+
+            <li><a href="<?php echo $menu ?>">Home</a></li>
+            <li><a href="#"> Access Forum</a></li>
+            <li><a href="utilisateur.php"> Your Account </a></li>
+            <li><a href="Contact.php"> Contact </a></li>		
+        </div>
+
+        <div class="sidenav"> <br>
+
+            <button id="close-image" name="img" onclick = "location.href = 'Recherche_seq.php'"> <img src="https://www.biospectrumasia.com/uploads/articles/oncotest-debiopharm-identify-biomarker-candidates.jpg" height="70" width="115"><br> Rechercher séquence </button> <br>
+            <button id="close-image" name="img" onclick = "location.href = 'Recherche_gen.php'"> <img src="https://www.biospectrumasia.com/uploads/articles/oncotest-debiopharm-identify-biomarker-candidates.jpg" height="70" width="115"><br> Rechercher génome </button> <br>
+            <button id="close-image" name="img" onclick = "location.href = 'PageRecherche.html'"> <img src="http://ugene.unipro.ru/wp-content/uploads/2015/03/55.png" height="70" width="115""><br> Alignement </button> <br>
+            <button id="close-image" name="img" onclick = "location.href = 'Sequence.php'"> <img src="https://i2.wp.com/bioinfo-fr.net/wp-content/uploads/2012/05/INSL5.png?ssl=1" height="70" width="115"><br> Base Nucléotidique </button> <br>
+            <button id="close-image" name="img" onclick = "location.href = 'Sequence.php'"> <img src="https://cdn.rcsb.org/rcsb-pdb/general_information/releases/1504_images/VisualizationStructure10000.png" height="70" width="115"><br> Base Proteique </button> <br>
+            <button id="close-image" name="img" onclick = "location.href = 'Genome.php'"> <img src="https://genome.cshlp.org/content/19/10/1801/F1.large.jpg" height="70" width="115"><br> Base Génome </button> <br>
+        
+	    </div>
+
+        <table style='width:40%';>
+	        <div id="pageresults">
+	            <h2> Résultats :</h2>
+                <?php
+                //Connexion a la base de donnee
+                $db = pg_connect( "host=localhost port=5432 dbname=genegate user=abirami password=16011996"  );
+                if(!$db) {      
+                    echo "Error : Unable to open database\n";
+                }
+                //Recuperation de l'id du sujet
+                $str=$_SERVER['REQUEST_URI']; 
+	            $keywords = preg_split("/=/", $str);
+	            $id_sujet = $keywords[1]; // id sujet recupéré de l'url
+                //Requete sql pour savoir quelles reponses affichees
+                $res = pg_query($db,"SELECT reponse.dateReponse, reponse.nomAnnot, reponse.response FROM genegate.reponse WHERE reponse.idSujet = '".$id_sujet."' ORDER BY reponse.dateReponse ASC;");
+                if (!$res) {
+                    echo "Une erreur s'est produite.<br>";
+                    exit;
+                }
+                //Si aucun resulat
+                if(pg_num_rows($res) == 0) {
+                    echo "Aucune discussion a afficher.<br>";
+                }
+                //Si il y a des discussions a afficher
+                if(pg_num_rows($res) != 0) {
+                    echo " <td colspan='3'> Date_Reponse Identifiant_Annot Reponse </td>";
+                    while ($row = pg_fetch_assoc($res) ){
+                        echo "<br><tr>
+                        <td>".$row['dateReponse']."</a> </td>  
+                        <td>".$row['nomAnnot']."</td>
+                        <td>".$row['response']."</td>
+                        </tr>";
+                    
+                    }
+                }
+                //Deconnexion
+                pg_close($db);
+                ?>
+        </table>
+            </div>
+        
+        <input type="button" class="button_active" onclick="location.href='ForumReponse.php?id=<?php echo $id_sujet;?>';" value="Repondre"/>
+
+
+
+</body>
+
+	<div class ="bottombar">
+  		<li><a href="plan.php"> Plan du Site  </a> </li>
+  		<li><a href="policy.php"> Conditions  </a> </li>
+  		<li><a href="About us.php"> Qui sommes nous ? </a> </li>
+  		<li><a href="Contact.php"> Nous contacter </a></li> 
+	</div> 
+
+
+	<div id = "footer"> 
+		© 2020 GeneGATE
+	</div> 
+
+</html>
---
+++ ForumSujets.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<?php session_start();?>
+<html lang="fr">
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Website Style</title>  
+	    <link rel="stylesheet" type="text/css" href="style1.css">
+	    <div id="header"> <br>
+		    <h> GeneGATE </h> <br><br>					
+	    </div>
+    </head>
+
+    <body>
+        <div id ="menu"> 
+                <?php if ( $_SESSION['statut'] == 'Annotateur') {
+                    $menu='MenuA.php'; 
+            } else if ( $_SESSION['statut'] == 'Validateur') {      	 
+                $menu='MenuV.php'; 
+            } else {
+                $menu='MenuL.php'; 
+            }?>
+
+            <li><a href="<?php echo $menu ?>">Home</a></li>
+            <li><a href="#"> Access Forum</a></li>
+            <li><a href="utilisateur.php"> Your Account </a></li>
+            <li><a href="Contact.php"> Contact </a></li>		
+        </div>
+
+        <div class="sidenav"> <br>
+
+            <button id="close-image" name="img" onclick = "location.href = 'Recherche_seq.php'"> <img src="https://www.biospectrumasia.com/uploads/articles/oncotest-debiopharm-identify-biomarker-candidates.jpg" height="70" width="115"><br> Rechercher séquence </button> <br>
+            <button id="close-image" name="img" onclick = "location.href = 'Recherche_gen.php'"> <img src="https://www.biospectrumasia.com/uploads/articles/oncotest-debiopharm-identify-biomarker-candidates.jpg" height="70" width="115"><br> Rechercher génome </button> <br>
+            <button id="close-image" name="img" onclick = "location.href = 'PageRecherche.html'"> <img src="http://ugene.unipro.ru/wp-content/uploads/2015/03/55.png" height="70" width="115""><br> Alignement </button> <br>
+            <button id="close-image" name="img" onclick = "location.href = 'Sequence.php'"> <img src="https://i2.wp.com/bioinfo-fr.net/wp-content/uploads/2012/05/INSL5.png?ssl=1" height="70" width="115"><br> Base Nucléotidique </button> <br>
+            <button id="close-image" name="img" onclick = "location.href = 'Sequence.php'"> <img src="https://cdn.rcsb.org/rcsb-pdb/general_information/releases/1504_images/VisualizationStructure10000.png" height="70" width="115"><br> Base Proteique </button> <br>
+            <button id="close-image" name="img" onclick = "location.href = 'Genome.php'"> <img src="https://genome.cshlp.org/content/19/10/1801/F1.large.jpg" height="70" width="115"><br> Base Génome </button> <br>
+        
+	    </div>
+
+        <table style='width:40%';>
+	        <div id="pageresults">
+	            <h2> Résultats :</h2>
+                <?php
+                //Connexion a la base de donnee
+                $db = pg_connect( "host=localhost port=5432 dbname=genegate user=abirami password=16011996"  );
+                if(!$db) {      
+                    echo "Error : Unable to open database\n";
+                }
+                //Recuperation de l'id de l'utilisateur
+                $id_utilisateur = $_SESSION['id_utilisateur'];
+                //Requete sql pour savoir quelles discussions affichees
+                $res = pg_query($db,"SELECT forum.idSujet, forum.sujet, forum.nomAnnot, forum.dateCreation FROM genegate.forum, genegate.accessujet WHERE accessujet.nomAnnot = '".$id_utilisateur."' AND forum.idSujet = forum.idSujet ORDER BY forum.dateCreation DESC;");
+                if (!$res) {
+                    echo "Une erreur s'est produite.<br>";
+                    exit;
+                }
+                //Si aucun resulat
+                if(pg_num_rows($res) == 0) {
+                    echo "Aucune discussion a afficher.<br>";
+                }
+                //Si il y a des discussions a afficher
+                if(pg_num_rows($res) != 0) {
+                    echo " <td colspan='3'> Sujet Identifiant_Creation Date_Creation </td>";
+                    while ($row = pg_fetch_assoc($res) ){ //On affiche les sujets ligne par ligne
+                        echo "<br><tr>
+                        <td> <a href='ForumDiscussions.php?id=".$row['idSujet']."'> ".$row['sujet']."</a> </td>  
+                        <td>".$row['nomAnnot']."</td>
+                        <td>".$row['dateCreation']."</td>
+                        </tr>";
+                    
+                    } //"id= " permet de savoir sur quel sujet affiche sur la page sujet
+                }
+                //Deconnexion
+                pg_close($db);
+                ?>
+        </table>
+            </div>
+        
+        <input type="button" class="button_active" onclick="location.href='ForumCreationSujet.php';" value="Nouveau Sujet"/>
+
+
+
+</body>
+
+	<div class ="bottombar">
+  		<li><a href="plan.php"> Plan du Site  </a> </li>
+  		<li><a href="policy.php"> Conditions  </a> </li>
+  		<li><a href="About us.php"> Qui sommes nous ? </a> </li>
+  		<li><a href="Contact.php"> Nous contacter </a></li> 
+	</div> 
+
+
+	<div id = "footer"> 
+		© 2020 GeneGATE
+	</div> 
+
+</html>
