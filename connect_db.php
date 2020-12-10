3e2e94e582fa28e84c7ac9a92c3ae889535a6557
RJker <71787083+RJker@users.noreply.github.com>
2020-12-10 19:51:05 +0100

    Update connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -1,6 +1,9 @@
 <?php
   //Selectionner la bonne connexion
+  //--------------------------------
+
   //$db = pg_connect("host=localhost port=5432 dbname=genegate username =postgres") or die ("Connection failed");
-  //$db = pg_connect("host=localhost dbname=romane user=romane") or die ("Connection failed");
-  $db = pg_connect("host=localhost port=5432 dbname=genegate user=abirami password=16011996") or die ("Connection failed");
+  $db = pg_connect("host=localhost dbname=romane user=romane") or die ("Connection failed");
+  //$db = pg_connect("host=localhost port=5432 dbname=genegate user=abirami password=16011996") or die ("Connection failed");
+
 ?>
