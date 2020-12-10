99ca07d1d57b41116d46c3cfa6e987058f851836
PatienceComfort <30979828+PatienceComfort@users.noreply.github.com>
2020-12-10 06:39:11 +0100

    Add files via upload

--- admini.php
+++ admini.php
@@ -6,25 +6,27 @@
   </head>
   <body>
     <h2>Utilisateurs à valider</h2>
-    <table>
+    <table border = "2">
       <tr>
         <td>Username</td>
         <td>email</td>
+        <td>Role demandé</td>
         <td>Valider</td>
       </tr>
       <?php
       include "connect_db.php";
-      $infos = pg_query($bdd,"SELECT username,email, validation_compte FROM utilisateur");
+      $infos = pg_query($bdd,"SELECT username,email, statut,validation_compte FROM utilisateur");
       while($data = pg_fetch_array($infos))
       {?>
         <tr>
           <td><?php echo $data['username'];?></td>
           <td><?php echo $data['email'];?></td>
+          <td><?php echo$data['statut'];?></td>
           <td><a href="edit.php?validation_compte=<?php echo $data['validation_compte'];?>">Confirmer</a></td>
         </tr>
       <?php
       }
       ?>
-    </table>
+    </table><br>
   </body>
 </html>
