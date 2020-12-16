076d4c7d2cde096d2650f7353b21e772b24da991
RJker <71787083+RJker@users.noreply.github.com>
2020-12-16 08:55:04 +0100

    Update fiche2.php

--- fiche2.php
+++ fiche2.php
@@ -65,6 +65,7 @@ include 'connect_db.php';
 		<tr><th> Genome ID </th><td><a href='fiche.php?id=".$row['idgenome']."'> ".$row['idgenome']."</a></td></tr>
 	    <tr><th> Biotype gene </th><td>".$row['biotypegene']."</td></tr>
 		<tr><th> Biotype transcrit </th><td>".$row['biotypetranscrit']."</td></tr>
+		<tr><th> Description </th><td>".$row['fonction']."</td></tr>
 		<tr><th> position début </th><td>".$row['pos_debut']."</td></tr>
 		<tr><th> position fin </th><td>".$row['pos_fin']."</td></tr>
 	    <tr><th> statut </th><td>".$row['annotee']."</td></tr>";
